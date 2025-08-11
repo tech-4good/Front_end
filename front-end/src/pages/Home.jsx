517de9c77551013b5c56a515ac7f1d58e71a8cdd
Bruna Santana Reginato <b.santana.reginato@avanade.com>
2025-08-11 17:04:17 -0300

    feat(pages|styles): arrumei o lugar onde as pages Cadastro.jsx, Login.jsx, Home.jsx devem ficar.

---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import Card from "../../components/Card/Card";
+import "./Home.css";
+
+import iconEndereco from "../../assets/casa.png";
+import iconBeneficiados from "../../assets/users.png";
+import iconEntregar from "../../assets/entrega.png";
+import iconConsultar from "../../assets/lupa.png";
+import iconRelatorio from "../../assets/painel.png";
+import iconHistorico from "../../assets/historico.png";
+import iconCestas from "../../assets/caixa-aberta.png";
+import iconVoluntarios from "../../assets/user.png";
+
+const cards = [
+  {
+    title: "Cadastrar Endereço",
+    icon: (
+      <img src={iconEndereco} alt="Cadastrar Endereço" width={44} height={44} />
+    ),
+    onClick: () => {},
+  },
+  {
+    title: "Cadastrar Beneficiados",
+    icon: (
+      <img
+        src={iconBeneficiados}
+        alt="Cadastrar Beneficiados"
+        width={44}
+        height={44}
+      />
+    ),
+    onClick: () => {},
+  },
+  {
+    title: "Entregar Cesta",
+    icon: (
+      <img src={iconEntregar} alt="Entregar Cesta" width={44} height={44} />
+    ),
+    onClick: () => {},
+  },
+  {
+    title: "Consultar Beneficiados",
+    icon: (
+      <img
+        src={iconConsultar}
+        alt="Consultar Beneficiados"
+        width={44}
+        height={44}
+      />
+    ),
+    onClick: () => {},
+  },
+  {
+    title: "Relatório",
+    icon: <img src={iconRelatorio} alt="Relatório" width={44} height={44} />,
+    onClick: () => {},
+  },
+  {
+    title: "Histórico de Cestas",
+    icon: (
+      <img
+        src={iconHistorico}
+        alt="Histórico de Cestas"
+        width={44}
+        height={44}
+      />
+    ),
+    onClick: () => {},
+  },
+  {
+    title: "Cestas",
+    icon: <img src={iconCestas} alt="Cestas" width={44} height={44} />,
+    onClick: () => {},
+  },
+  {
+    title: "Voluntários",
+    icon: (
+      <img src={iconVoluntarios} alt="Voluntários" width={44} height={44} />
+    ),
+    onClick: () => {},
+  },
+];
+
+const Home = () => {
+  return (
+    <div className="home-page">
+      <NavBar userName="Aline" />
+      <div className="home-content">
+        <h1 className="home-title">Central de Acesso</h1>
+        <p className="home-subtitle">
+          Escolha uma das opções abaixo para acessar as informações desejadas
+        </p>
+        <div className="card-grid">
+          {cards.map((card, idx) => (
+            <Card
+              key={idx}
+              title={card.title}
+              icon={card.icon}
+              onClick={card.onClick}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
