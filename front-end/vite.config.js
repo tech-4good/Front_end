153a152ba0f23ffa3520d371252f28223e022bd6
fernandofs21 <fernando.silva@sptech.school>
2025-10-28 17:29:17 -0300

    feat: implement proper proxy reverso with envsubst for dynamic API IP

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -2,6 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    }
+  }
 })
