d42efb146bb94a35a4d00bbd17a3a301d68eb64d
BrunaReginat0 <bruna.reginato@sptech.school>
2025-11-24 17:41:42 -0300

    testes

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(() => {
     server: {
       proxy: {
         "/api": {
-          target: "http://192.168.15.2:8080",
+          target: "http://localhost:8080",
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/api/, ""),
